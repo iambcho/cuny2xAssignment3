f8b3f3872f71f656c44d2e75e1afcfd49275fb71
Brittany Cho <bac411@nyu.edu>
2020-01-06 13:59:36 -0500

    prompts 5-8

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ Write the code necessary to do the following:
 8) Create a new li element.
 9) Give the li the text "four".
 10) Append the li to the ul element.
-12) Loop over all of the lis inside the ol tag and give them a background color of "green".
-13) Remove the div with a class of footer.
+11) Loop over all of the lis inside the ol tag and give them a background color of "green".
+12) Remove the div with a class of footer.
 
 
 */
@@ -54,6 +54,29 @@ let third2 = third1.lastElementChild;
 console.log(third2);
 
 // 5) Give the section with an id of container the text "Hello!".
+let p = document.createElement("p")                
+let text = document.createTextNode("Hello!");     
+p.appendChild(text); 
+container1.appendChild(p);
+
 // 6) Add the class main to the div with a class of footer.
+
+let footer = document.querySelector(".footer");
+footer.className += " main";
+
 // 7) Remove the class main on the div with a class of footer.
+footer.classList.remove("main");
+
 // 8) Create a new li element.
+
+let newLi = document.createElement("li");
+
+// 9) Give the li the text "four".
+// NOTE: you need to use the element created in #8 to do #9 and #10
+
+// 10) Append the li to the ul element.
+
+// 11) Loop over all of the lis inside the ol tag and give them a background color of "green".
+
+// 12) Remove the div with a class of footer.
+

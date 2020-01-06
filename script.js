15733cd6627a0cfbc314538c66fc8f1010a0927b
Brittany Cho <bac411@nyu.edu>
2020-01-06 13:44:43 -0500

    prompts 1-4

--- script.js
+++ script.js
@@ -29,5 +29,31 @@ Write the code necessary to do the following:
 1) Select the section with an id of container without using querySelector.
 */
 
-let container = document.getElementById('container');
-console.log(container);
+let container1 = document.getElementById("container");
+// console.log(container1);
+console.log(document.getElementById("container"));
+
+// console.log(document.getElementById("test"));
+
+
+//2) Select the section with an id of container using querySelector.
+
+let container2 = document.querySelector("#container");
+console.log(container2);
+
+//3) Select all of the list items with a class of "second".
+
+let second = document.getElementsByClassName("second");
+console.log(second);
+
+//4) Select a list item with a class of third, but only the list item inside of the ol tag.
+
+let third1 = document.querySelector("ol");
+let third2 = third1.lastElementChild;
+
+console.log(third2);
+
+// 5) Give the section with an id of container the text "Hello!".
+// 6) Add the class main to the div with a class of footer.
+// 7) Remove the class main on the div with a class of footer.
+// 8) Create a new li element.
